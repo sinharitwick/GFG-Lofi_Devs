4ae925b756caaca271c8d25ad30b5236b458cb73
ritz <sritwick10@gmail.com>
2023-04-15 00:43:42 +0530

    Feed page changes

--- client/src/components/UI/Landing.js
+++ client/src/components/UI/Landing.js
@@ -1,16 +1,19 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect } from 'react'
 import Navbar from './Navbar'
 import { Link } from 'react-router-dom'
 import Typewriter from "typewriter-effect"
 import './Landing.css'
 import Contact from './Landing/Contact'
+import About from './Landing/About'
+import Fade from 'react-reveal/Fade';
 
 function Landing() {
   return (
     <div>
         <Navbar />
-        <div class="container-landing">
-            <div class="left">
+        <Fade bottom>
+        <div className="container-landing">
+            <div className="left">
                 <h1 id="msg">
                   <Typewriter 
                   options={{
@@ -26,10 +29,12 @@ function Landing() {
                 <p id="quote">"The ultimate goal of farming is not the growing of crops, but the cultivation and perfection of human beings." - Masanobu Fukuoka</p>
                 <p className='Fbtn'><Link to="/feed"><button>Explore</button></Link></p>
             </div>
-            <div class="right">
+            <div className="right">
                 <img src={process.env.PUBLIC_URL + '/heroimg.png'} alt="" id="heroimg" />
             </div>
         </div>
+        </Fade>
+        <About />
         <Contact />
     </div>
   )
--- client/src/components/UI/Landing/Contact.js
+++ client/src/components/UI/Landing/Contact.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
 
 function Contact() {
   return (
     <div name='contact' className='w-full h-screen bg-[#c5d79f] flex justify-center items-center p-4'>
+        <Fade bottom>
         <form action="https://getform.io/f/225ffde8-777b-456c-afc5-56e323ec0b65" method='POST' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#155705]'>Contact</p>
@@ -13,6 +15,7 @@ function Contact() {
             <textarea className='p-2' name="message" rows="10" placeholder='Write your message'></textarea>
             <button className="border-2 flex px-4 py-3 my-4 mx-auto items-center hover:border-[#155705] duration-300">Submit</button>
         </form>
+        </Fade>
     </div>
   )
 }
