bb0045b930d3a6d3a418abf25f993ba846073df7
ritz <sritwick10@gmail.com>
2023-04-16 03:08:07 +0530

    Navbar bugs fixed finally

--- client/src/components/UI/Landing.js
+++ client/src/components/UI/Landing.js
@@ -1,5 +1,5 @@
 import {React, useState, useEffect } from 'react'
-import Navbar from './Navbar'
+import Navbar from '../UI/Navbar'
 import { Link } from 'react-router-dom'
 import Typewriter from "typewriter-effect"
 import './Landing.css'
